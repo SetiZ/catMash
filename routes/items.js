c10b98d1d9b3f59ec1b69d6340453bcb75a6a0d4
SetiZ <SetiZ@users.noreply.github.com>
2020-12-15 14:43:08 +0100

    feat: add update feature for db

--- routes/items.js
+++ routes/items.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 const fetch = require('node-fetch');
-// import { insert, select, update, remove, file } from "easy-db-node";
 const easyDb = require("easy-db-node");
 
 async function getCats() {
@@ -19,52 +18,40 @@ async function getRandomCat() {
     return randomCat;
 }
 
+async function updateCat(id) {
+    const cat = await easyDb.select("cats", id);
+    await easyDb.update("cats", id, { ...cat, score: cat.score + 1 });
+}
+
+// GET ALL
 router.get('/', function (req, res) {
     getCats().then(cats => {
         res.status(200).json(cats);
+    }).catch(err => {
+        console.error(err);
+        res.sendStatus(404);
     })
 });
 
+// RANDOM
 router.get('/random', function (req, res) {
     getRandomCat().then(cat => {
         res.status(200).json(cat);
     }).catch(err => {
-        console.err(err);
+        console.error(err);
         res.sendStatus(404);
     })
 });
 
 // UPDATE
-// this api end-point update an existing item object
-// for that we get `id` and `title` from api end-point of item to update
 router.put('/:id', function (req, res) {
-    // get item object match by `id`
-    let found = data.find(function (item) {
-        return item.id === parseInt(req.params.id);
-    });
-
-    // check if item found
-    if (found) {
-        let updated = {
-            id: found.id,
-            title: req.body.title, // set value of `title` get from req
-            order: req.body.order, // set value of `order` get from req
-            completed: req.body.completed // set value of `completed` get from req
-        };
-
-        // find index of found object from array of data
-        let targetIndex = data.indexOf(found);
-
-        // replace object from data list with `updated` object
-        data.splice(targetIndex, 1, updated);
-
-        // return with status 204
-        // success status response code 204 indicates
-        // that the request has succeeded
-        res.sendStatus(204);
-    } else {
+    updateCat(req.params.id).then(cat => {
+        // console.log(cat)
+        res.status(204).json(cat);
+    }).catch(err => {
+        console.error(err);
         res.sendStatus(404);
-    }
+    })
 });
 
 module.exports = router;
