ac71f2df3a845532e53f8d3ef22ff977b93c39e7
SetiZ <SetiZ@users.noreply.github.com>
2020-12-20 00:30:18 +0100

    feat: add cors to work on react version

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const http = require('http');
 const path = require('path');
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 
 const itemsRouter = require('./routes/cats');
 
 const app = express();
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
-// app.use(cors({origin: 'http://localhost:8100'}));
+app.use(cors({origin: 'http://localhost:3001'}));
 
 app.use('/cats', itemsRouter);
 
